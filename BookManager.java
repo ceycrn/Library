803f20b5f09ed6d660fcfdac34888f1dfb3ac6d4
ceycrn <ceyda-cerenn@hotmail.com>
2023-05-03 22:46:34 +0300

    Update BookManager.java

--- BookManager.java
+++ BookManager.java
@@ -23,4 +23,11 @@ public class BookManager {
         book.setISBN(isbn);
         ;
     }
+
+    public void borrow(User user, Book book, int date) {
+        System.out.println("-Name of the person: " + user.getName() + " " + user.getMidname() + " -"
+                + "Borrowed this book: " + book.getBookName());
+        System.out.println("Borrow date: " + date + ".04.2023");
+        System.out.println("Return date: " + (date + 7) + ".04.2023");
+    }
 }
